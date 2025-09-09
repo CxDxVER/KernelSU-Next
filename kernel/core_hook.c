a500ec4d20da1aa39e4d7311dcd41da98d91320a
Vergil <caesar.x.julius@gmail.com>
2025-09-09 19:58:56 +0530

    Revert "Reset seccomp filter count when escaping to root (https://github.com/tiann/KernelSU/pull/2708) (#743)"
    This reverts commit a540992e9b6395a245a3448e78f19e87d4ea3437.

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -131,7 +131,6 @@ static void disable_seccomp(void)
 #ifdef CONFIG_SECCOMP
 	current->seccomp.mode = 0;
 	current->seccomp.filter = NULL;
-	atomic_set(&current->seccomp.filter_count, 0);
 #else
 #endif
 }
